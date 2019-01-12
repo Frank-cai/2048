1d7a4581a46efeb6ffea6bd880072bff26bfc7d4
Frank-cai <40657099+Frank-cai@users.noreply.github.com>
2019-01-13 04:26:13 +0800

    Add files via upload

--- 2048/evaluate.py
+++ 2048/evaluate.py
@@ -5,7 +5,6 @@ import torch
 
 def single_run(size, score_to_win, AgentClass, **kwargs):
     game = Game(size, score_to_win)
-    # agent = AgentClass(game, display=Display(), **kwargs)
     agent.play(verbose=False, train=0)
     return game.score
 
@@ -27,28 +26,22 @@ if __name__ == '__main__':
     agent = TestAgent(game, display=Display())
 
     scores = []
-    # agent.net8.load_state_dict(torch.load("net_3_params_8.pkl", map_location='cpu'))
     agent.net7.load_state_dict(torch.load("net_3_params_7.pkl", map_location='cpu'))
     agent.net6.load_state_dict(torch.load("net_3_params_6.pkl", map_location='cpu'))
     agent.net5.load_state_dict(torch.load("net_3_params_5.pkl", map_location='cpu'))
     agent.net4.load_state_dict(torch.load("net_3_params_4.pkl", map_location='cpu'))
-    # agent.net3.load_state_dict(torch.load("net_3_params_3.pkl", map_location='cpu'))
     agent.net2.load_state_dict(torch.load("net_3_params_2.pkl", map_location='cpu'))
-    # agent.net1.load_state_dict(torch.load("net_3_params_1.pkl", map_location='cpu'))
 
     step = 0
     for i in range(N_TESTS):
         game = Game(size, score_to_win)
         agent = TestAgent(game, display=Display())
 
-        # agent.net8.load_state_dict(torch.load("net_3_params_8.pkl", map_location='cpu'))
         agent.net7.load_state_dict(torch.load("net_3_params_7.pkl", map_location='cpu'))
         agent.net6.load_state_dict(torch.load("net_3_params_6.pkl", map_location='cpu'))
         agent.net5.load_state_dict(torch.load("net_3_params_5.pkl", map_location='cpu'))
         agent.net4.load_state_dict(torch.load("net_3_params_4.pkl", map_location='cpu'))
-        # agent.net3.load_state_dict(torch.load("net_3_params_3.pkl", map_location='cpu'))
         agent.net2.load_state_dict(torch.load("net_3_params_2.pkl", map_location='cpu'))
-        # agent.net1.load_state_dict(torch.load("net_3_params_1.pkl", map_location='cpu'))
 
         agent.play(verbose=True)
         step = step + agent.n_iter
--- 2048/webapp.py
+++ 2048/webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-
+import torch
 
 def get_flask_app(game, agent):
     app = Flask(__name__)
@@ -37,8 +37,14 @@ if __name__ == "__main__":
     game = Game(size=GAME_SIZE, score_to_win=SCORE_TO_WIN)
 
     try:
-        from game2048.agents import ExpectiMaxAgent
-        agent = ExpectiMaxAgent(game=game)
+        from game2048.agents import MyAgent
+        agent = MyAgent(game=game)
+        agent.net7.load_state_dict(torch.load("net_3_params_7.pkl", map_location='cpu'))
+        agent.net6.load_state_dict(torch.load("net_3_params_6.pkl", map_location='cpu'))
+        agent.net5.load_state_dict(torch.load("net_3_params_5.pkl", map_location='cpu'))
+        agent.net4.load_state_dict(torch.load("net_3_params_4.pkl", map_location='cpu'))
+        agent.net2.load_state_dict(torch.load("net_3_params_2.pkl", map_location='cpu'))
+
     except:
         from game2048.agents import RandomAgent
         print("WARNING: Please compile the ExpectiMaxAgent first following the README.")
@@ -49,5 +55,4 @@ if __name__ == "__main__":
     
     app = get_flask_app(game, agent)
     app.run(port=APP_PORT, threaded=False, host=APP_HOST)  # IMPORTANT: `threaded=False` to ensure correct behavior
-    
-    
+
